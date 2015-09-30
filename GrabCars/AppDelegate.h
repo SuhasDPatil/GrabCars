350d4880cc260915602afd07d3f9646b50e1ad41
SuhasDPatil <rac@Rac.local.lan>
2015-09-30 10:41:54 +0530

    Initial Commit

---
+++ GrabCars/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GrabCars
+//
+//  Created by rac on 30/09/15.
+//  Copyright (c) 2015 Suhas Patil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
